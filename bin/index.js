8da75a23a7a6fbf1c8eb796e8550cac4bc387e71
R Surahutomo Aziz Pradana <pradana@Rs-MacBook-Pro.local>
2021-08-29 02:08:04 +0700

    Initial Project

---
+++ bin/index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const colors = require('colors');
+const greet = require("../lib/greet");
+
+// print random greeting
+console.log(
+    // wraps text with rainbow color formatting
+    colors.rainbow(
+        // returns the random greeting text
+        greet.greetRandom()
+    ),
+    colors.rainbow(
+        // returns the random greeting text
+        greet.greet('en')
+    )
+);
